1bf4eed96fab7d339ee59e1e0b9e4f0d050b7a3b
Martinserrano724 <martinserrano724@gmail.com>
2023-05-24 11:28:10 -0300

    Desarrollo del punto 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from 'react'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import Ejercicio3 from './components/Ejercicio3'
 
 function App() {
-
+  const [msjExtra,setMensaje] =useState('')
+  function msj(){
+    return (setMensaje('(from changed state)!'))
+  }
 
   return (
     <>
-      
+   
+    <Ejercicio3 tituloExtra='My friend' msj={msjExtra} className=" "/>
+    <div className='d-flex justify-content-center'>
+     <button className='btn btn-primary ' onClick={msj}>Click</button>
+    
+    </div>
+          
     </>
   )
 }
---
+++ src/components/Ejercicio3.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Ejercicio3 = ({tituloExtra,msj}) => {
+    return (
+        <div className=' mt-1 '>
+            <h3 className=' mt-5 text-center'>Hola {tituloExtra} {msj}</h3>
+        </div>
+    );
+};
+
+export default Ejercicio3;
